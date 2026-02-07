0820fc7706cbbf6bdf003d177546806e47d245b9
Daniel Baker <dan@djacu.dev>
2026-02-07 11:18:16 -0800

    community/2025/process.py: add years coding

--- community/2025/process.py
+++ community/2025/process.py
@@ -283,6 +283,33 @@ app = pn.Column(
                 """,
         plot_pane=make_simple_bar_chart_pane(df, 8),
     ),
+    pn.Spacer(height=20),
+    make_plot_row(
+        md_text="""
+                # Years Coding
+                """,
+        plot_pane=make_simple_bar_chart_pane(
+            df,
+            9,
+            label_order=[
+                "Less than 1 year",
+                "1 to 4 years",
+                "5 to 9 years",
+                "10 to 14 years",
+                "15 to 19 years",
+                "20 to 24 years",
+                "25 to 29 years",
+                "30 to 34 years",
+                "35 to 39 years",
+                "40 to 44 years",
+                "45 to 49 years",
+                "More than 50 years",
+                "I have never programmed",
+                "Prefer not to say",
+                "Skipped",
+            ],
+        ),
+    ),
     sizing_mode="stretch_width",
     margin=20,
 )
