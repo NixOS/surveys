25895b11bf8e7ddbe83d1f73b2158e31a3d85661
Daniel Baker <dan@djacu.dev>
2026-02-07 23:12:21 -0800

    community/2025/process.py: add experimental features row

--- community/2025/process.py
+++ community/2025/process.py
@@ -508,6 +508,13 @@ app = pn.Column(
             ],
         ),
     ),
+    pn.Spacer(height=20),
+    make_plot_row(
+        md_text="""
+                # Experimental Features
+                """,
+        plot_pane=make_multi_bar_chart_pane(df, 46, 70),
+    ),
     sizing_mode="stretch_width",
     margin=20,
 )
